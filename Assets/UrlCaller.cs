5fa5c11a5bb0460f4e65136976019e9b84e48938
Joe <51638822+tso996@users.noreply.github.com>
2022-04-23 19:47:25 +0100

    local to remote

---
+++ Assets/UrlCaller.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniGLTF;
+
+public class UrlCaller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log (Application.persistentDataPath);
+		//Debug.Log(android.content.Context.getExternalFilesDir);
+		StartCoroutine(ImportObject());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator ImportObject () {
+
+		WWW www = new WWW ("https://s3.ap-south-1.amazonaws.com/unitear/models/20190328145644.glb");
+		yield return www;
+
+	
+		if (!string.IsNullOrEmpty(www.error)) {
+		
+			Debug.Log(www.error);
+		
+		}else
+			{
+			string write_path = Application.persistentDataPath + "Model.glb";
+
+			System.IO.File.WriteAllBytes (write_path, www.bytes);
+
+			Debug.Log ("Wrote to path");
+
+			var path = write_path; // this must lead to the location of the glb file
+
+			var context = gltfImporter.Load(path);
+			context.ShowMeshes();
+
+			GameObject root = context.Root;
+			root.transform.SetParent (this.gameObject.transform);
+			//Transform[] t = this.gameObject.GetComponentsInChildren<Transform> ();
+			RestartAnim();
+		}
+	}
+
+	void RestartAnim(){
+		//to work around the bug of animation not playing, find current loaded animation clip in the animation component and play it again
+		this.transform.GetChild (0).gameObject.GetComponent<Animation>().Play(this.transform.GetChild (0).gameObject.GetComponent<Animation>().clip.name);
+	
+
+	}
+}
---
+++ Assets/gltfLoader.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniGLTF;
+
+public class gltfLoader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		// this path will probably need to point to the location in the device running the UniteAR app where the glb file is fetched from the link to the db
+		// It could also simply be a https link and could be used with a parser..not tested.
+		var path = "C:\\Users\\asus\\Desktop\\GltfCovertedFiles\\Junkrt.glb"; // gltf, glb or zip(include gltf)
+
+		var context = gltfImporter.Load(path);
+		context.ShowMeshes();
+
+		GameObject root = context.Root;
+		root.transform.SetParent (this.gameObject.transform);
+		//Transform[] t = this.gameObject.GetComponentsInChildren<Transform> ();
+		RestartAnim();
+	
+	}
+
+	void RestartAnim(){
+	//to work around the bug of animation not playing.Find current loaded animation clip in the animation component and play it again
+		//this may not be required when using an animator
+		if (this.transform.GetChild (0).gameObject.GetComponent<Animation> () != null) {
+			AnimationClip a = this.transform.GetChild (0).gameObject.GetComponent<Animation> ().clip;
+			string animationToPlay = a.name;
+			this.transform.GetChild (0).gameObject.GetComponent<Animation> ().Play (animationToPlay);
+		}
+	
+	}
+	
+
+}
